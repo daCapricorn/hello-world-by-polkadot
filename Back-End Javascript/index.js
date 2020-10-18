e4633171b1f1b3017cdd876a95d4e2235310f957
Steven Wu <capricorn0113@gmail.com>
2020-10-18 12:35:32 +0800

    better ui

--- Back-End Javascript/index.js
+++ Back-End Javascript/index.js
@@ -1,13 +1,18 @@
 const { ApiPromise, WsProvider } = require('@polkadot/api');
+const ora = require('ora');
 
 async function run() {
+  const spinner = ora('');
+  spinner.start();
+
   const wsProvider = new WsProvider('wss://rpc.polkadot.io');
   const api = await ApiPromise.create({ provider: wsProvider });
 
   const hash = await api.rpc.chain.getFinalizedHead();
+  spinner.text = `Fetching block info #${hash} …`;
   const body = await api.rpc.chain.getBlock(hash);
 
-  console.info(JSON.stringify(body, null, 2));
+  spinner.succeed(JSON.stringify(body, null, 2));
 }
 
 try {
